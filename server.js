69abf506daaeea6f5d2709c0c2d09326ad61a90c
ezechukwuji <ezechukwuji@gmail.com>
2018-04-28 16:41:51 +0100

    prep repo for heroku

--- server.js
+++ server.js
@@ -2,11 +2,20 @@ var express = require('express'); // grants access to the entire express API
 
 // create a new express app
 var app = express();
+const PORT = process.env.PORT || 3000;
 
 // Tell express which folder you want to serve
+
+app.use(function(req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'http'){
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 app.use(express.static('public'));
 
 // start the server
-app.listen(3000, function(){
-  console.log('Express server is running on port 3000');
+app.listen(PORT, function(){
+  console.log('Express server is running on port' +  PORT);
 })
