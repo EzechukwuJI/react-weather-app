86e84bf20bfaa7afd1c7cfa08466d6a1d68f458a
ezechukwuji <ezechukwuji@gmail.com>
2018-05-01 07:27:52 +0100

    added style to pages

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 
 var Examples = React.createClass({
   render: function(){
     return (
-      <h4>Nested Examples component</h4>
+      <div className="text-center">
+        <p className="text-center">Here are a few example locations to try out </p>
+        <ol>
+          <li><Link to="/location=lagos">Lagos, Nigeria</Link></li>
+          <li><Link to="/location=benin">Benin, Nigeria</Link></li>
+          <li><Link to="/location=moscow">Moscow, Russia</Link></li>
+          <li><Link to="/location=france">Paris, France</Link></li>
+        </ol>
+      </div>
     );
   }
 });
